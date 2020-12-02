dcbea9dba192bd3e799cb5e23cd86544c185c7da
bishnunathbd <bishnunath774@gmail.com>
2020-12-02 09:55:43 +0600

    facebook like btn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import { useState } from 'react';
 
 function App() {
+  const [likeColor, setLikeColor] = useState('');
+
+  const handleLike = () => {
+    // const color = likeColor ? '' : 'primary';
+    // setLikeColor(likeColor ? '' : 'primary');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Facebook Like Button</h1>
+      {/* <ThumbUpAltIcon onClick={handleLike} color={likeColor}></ThumbUpAltIcon> */}
+      <ThumbUpAltIcon onClick={() => setLikeColor(likeColor ? '' : 'primary')} color={likeColor}></ThumbUpAltIcon>
     </div>
   );
 }
